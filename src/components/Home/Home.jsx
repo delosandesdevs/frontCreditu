63ba974ccb527628ffc4e69a5dfdae1f0f44262e
Rodrigo <rodrigoperez0109@gmail.com>
2022-08-29 19:19:54 -0300

    top3_container

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import Player from '../Player/Player';
 import { API_PORT } from '../../redux/constans';
 import ImageTest from './test.png'
+import './Home.scss'
 
 const Home = (props) => {
 
@@ -54,11 +55,12 @@ const Home = (props) => {
     }, [])
 
     return (
-        <div>
-            {errorThree && <p>No top 3 were found</p>}
-            <ol>
-                {topthree && topthree.map(p => <li><Player name={p.name} img={p.img} ranking={p.ranking} alt={'topthree'} key={p.ranking} /></li>)}
-            </ol>
+        <div className='home_container'>
+            <div className='top-three_container'>
+                {/* {errorThree && <p>No top 3 were found</p>} */}
+                <h2>TOP 3</h2>
+                {topthree && topthree.map(p => <Player name={p.name} img={p.img} ranking={p.ranking} alt={'topthree'} key={p.ranking} />)}
+            </div>
 
             {errorTen && <p>No top 10 were found</p>}
             <ol>

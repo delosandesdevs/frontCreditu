32b5fdd51705a0a1bce0437dcb25ac25dc14db5c
Juano <juanocataldo@gmail.com>
2022-08-29 13:37:12 -0300

    J:Testings of: Home and CreatePlayer components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,40 @@ import React from 'react';
 import {useDispatch, useSelector} from 'react-redux'
 import{actionTest} from '../redux/action'
 import {useEffect} from 'react'
-
+import { useState } from 'react';
+import { Player } from './Player';
+import { API_PORT } from '../redux/constans';
 
 function Home(props) {
+    
+    //TESTING, NO BORRAR
+    const [topthree, setTopthree] = useState([])
+    const [topten, setTopten] = useState([])
+    const [errorThree, setErrorThree] = useState(false)
+    const [errorTen, setErrorTen] = useState(false)    
+    
+    //TESTING, NO BORRAR
+    const testingTopThree = ()=>{
+        fetch(`http://localhost:${API_PORT}/topthree`)
+        .then(data => data.json())
+        .then(res => setTopthree(res))
+        .catch(e => setErrorThree(true))
+    }
+
+    //TESTING, NO BORRAR
+    const testingTopTen = ()=>{
+        fetch(`http://localhost:${API_PORT}/topten`)
+        .then(data => data.json())
+        .then(res => setTopten(res))
+        .catch(e => setErrorTen(true))
+    }
+
+    useEffect(() => {
+        //TESTING, NO BORRAR
+        testingTopThree()
+        testingTopTen()
+    },[])
+
     const data = useSelector(state => state.test_state)
     const dispatch = useDispatch()
 
@@ -21,6 +52,20 @@ function Home(props) {
                 }
             </div>
             <button onClick={()=>dispatch(actionTest())}>Test</button>
+
+            <h1>Top 3</h1>
+            <hr />
+            {errorThree && <p>No top 3 were found</p>}
+            <ol>
+                {topthree && topthree.map(p => <li><Player name={p.name} img={p.img} ranking={p.ranking} alt={'topthree'} key={p.ranking} /></li>)}
+            </ol>
+
+            {errorTen && <p>No top 10 were found</p>}
+            <ol>
+                {topten && topten.map(p => <li><Player name={p.name} img={p.img} ranking={p.ranking} alt={'topten'} key={p.ranking} /></li>)}
+            </ol>
+
+            <hr />
         </div>
     );
 }

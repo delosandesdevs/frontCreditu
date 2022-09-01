22cf64f98312e44ef783726f13dfb987fdc5c55c
Juano <juanocataldo@gmail.com>
2022-09-01 13:39:49 -0300

    Scroll icon

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import homeLogo from '../../assets/miscellaneous/home_logo.png'
 import introImage from '../../assets/miscellaneous/home_wallpaper.png'
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { Parallax } from 'react-scroll-parallax';
-
+import './Scroll.scss'
 import './Hero.scss'
 
 
@@ -16,6 +16,16 @@ const Hero = () => {
                 <img data-aos="fade" data-aos-duration={3000} src={homeLogo} alt="home_logo_img" id='home-logo' />
             </div>
                 <img src={introImage} alt="home_img" id="home-wallpaper" />
+            <div className="scroll">
+                <div class="scrolldown">
+                    <div class="chevrons">
+                        <div class="chevrondown"></div>
+                        <div class="chevrondown"></div>
+                    </div>
+                </div>
+            </div>
+
+
             </Parallax>
             </ParallaxProvider>
         </>

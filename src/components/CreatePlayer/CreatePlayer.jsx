082406edcc499db5375c4017ec6b75aaf2be7908
Rodrigo <rodrigoperez0109@gmail.com>
2022-08-31 23:15:44 -0300

    styles create player

--- src/components/CreatePlayer/CreatePlayer.jsx
+++ src/components/CreatePlayer/CreatePlayer.jsx
@@ -44,49 +44,50 @@ const CreatePlayer = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [player])
 
-    return <>
-            
-        <div className="banner-title">
-            <h1>Create Player</h1>
-        </div>
-
-        <div className="cmp-create-player">
-            <div className="cmp-create-player-avatar">
-                <div className="cmp-create-player-avatar-background">
-                    <img src={defaultAvatar} alt="default_avatar_img" />                    
-                </div>
-                <span id="cmp-create-player-span">SELECT YOUR AVATAR</span>
+    return (
+        <div className="create-player">
+            <div className="banner-title">
+                <h1>Usa tu imaginación y crea tu jugador</h1>
             </div>
 
-            <div className="cmp-create-player-form">
-                
-                <div className="create-player-form-field">
-                    <label className="cmp-create-player-label" htmlFor="name">Player Name</label>
-                    <input type="text" placeholder="Enter your player name here" onChange={handlePlayer} name="name" id="name" />
+            <div className="cmp-create-player">
+                <div className="cmp-create-player-avatar">
+                    <div className="cmp-create-player-avatar-background">
+                        <img src={defaultAvatar} alt="default_avatar_img" />
+                    </div>
+                    <span id="cmp-create-player-span">SELECCIONÁ TU AVATAR</span>
                 </div>
 
-                <div className="create-player-form-field">
-                    <label className="cmp-create-player-label" htmlFor="status">Status</label>
-                    <select onChange={handlePlayer} name="status" id="status" defaultValue='Select'>
-                        <option disabled value='Select' ></option>
-                        <option value="bronce">Bronce</option>
-                        <option value="silver">Silver</option>
-                        <option value="gold">Gold</option>
-                    </select>                    
+                <div className="cmp-create-player-form">
+
+                    <div className="create-player-form-field">
+                        <label className="cmp-create-player-label" htmlFor="name">Nombre jugador</label>
+                        <input type="text" placeholder="Enter your player name here" onChange={handlePlayer} name="name" id="name" />
+                    </div>
+
+                    <div className="create-player-form-field">
+                        <label className="cmp-create-player-label" htmlFor="status">Seleccioná tu avatar</label>
+                        <select onChange={handlePlayer} name="status" id="status" defaultValue='Select'>
+                            <option disabled value='Select' ></option>
+                            <option value="1">Avatar 1</option>
+                            <option value="2">Avatar 2</option>
+                            <option value="3">Avatar 3</option>
+                        </select>
+                    </div>
+
+                    {/* <div className="create-player-form-field">
+                        <label className="cmp-create-player-label" htmlFor="ranking">Ranking</label>
+                        <input type="number" placeholder="Enteranking" onChange={handlePlayer} name="ranking" id="ranking" />
+                    </div> */}
+
+                    <button className="create-player-submit" onClick={createPlayer}>CREAR JUGADOR</button>
+
+
+                    {error && <p style={{ color: "white", fontWeight: "bold", marginTop: "10px" }}>All fields should be filled</p>}
                 </div>
-
-                {/* <div className="create-player-form-field">
-                    <label className="cmp-create-player-label" htmlFor="ranking">Ranking</label>
-                    <input type="number" placeholder="Enteranking" onChange={handlePlayer} name="ranking" id="ranking" />
-                </div> */}
-
-                <button className="create-player-submit" onClick={createPlayer}>CREATE PLAYER</button>
-
-                
-                {error && <p style={{ color: "white", fontWeight: "bold", marginTop:"10px" }}>All fields should be filled</p>}
             </div>
         </div>
-    </>
+    )
 }
 
 export default CreatePlayer

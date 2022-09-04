2a4b12a75bc430ee7fd455be561ddeb1f27839f2
Juano <juanocataldo@gmail.com>
2022-09-04 00:04:05 -0300

    A ver ahora LPM

--- src/components/CreatePlayer/CreatePlayer.jsx
+++ src/components/CreatePlayer/CreatePlayer.jsx
@@ -1,48 +1,98 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from 'react-router-dom';
 import './CreatePlayer.scss'
 import defaultAvatar from '../../assets/avatars/default.png'
 import 'animate.css';
 import Avatar from "../Avatar/Avatar";
+import { objHasNull } from "../../functions/validateForm";
+import Gallery from "../Profile/Gallery/Gallery";
+import { avatarList } from "../../functions/varsForDevelopment";
+import { useDispatch } from "react-redux";
+import { postPlayer } from "../../redux/action";
+// import party from "party-js";
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
 
 const CreatePlayer = () => {
 
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const [player, setPlayer] = useState({
-        name: "",
-        avatar: 0,
-        // ranking: null
+        nickname: "",
+        avatar: '',
+        score: '0'
     })
 
     const [error, setError] = useState(false)
 
+    // FUNCTION TO MATCH THE IMAGE NAME => avatar-01, avatar-02, ... , avatar-10
+    const checkIfAvatar = (e) => {
+        if(e.target.name === 'avatar'){
+            if(e.target.value < 10)
+            return '/images/avatar-0'+e.target.value+'.png'
+            else
+            return '/images/avatar-'+e.target.value+'.png'
+        }
+        return e.target.value        
+    }
+
     const handlePlayer = (e) => {
         setPlayer({
             ...player,
-            [e.target.name]: e.target.value
+            [e.target.name]: checkIfAvatar(e)
         })
-    }
-
-    const hasNull = (target) => {
-        for (var member in target) {
-            if (target[member] === '' || target[member] === 0)
-                return true;
-        }
-        return false;
     }
 
     const errorhandler = () => {
         setError(false)
-
-        if (hasNull(player))
+        if (objHasNull(player))
             setError(true)
     }
 
     const createPlayer = (e) => {
+        // party.confetti(e.target, {
+        //     count: party.variation.range(10, 20),
+        //   });
         errorhandler()
+        if(!error){
+            // dispatch(postPlayer(player))
+
+            Swal.fire({
+                title: '¿Estás seguro de que deseas crear tu Player?',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: '¡Estoy seguro!',
+              }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                  dispatch(postPlayer(player))
+                  Swal.fire({
+                    title:'¡Has creado tu Player con éxito!',
+                    icon:'success',
+                    confirmButtonText: 'Continuar'
+                  })
+                  navigate('/')
+                } else if (result.isDismissed) {
+                }
+              })
+
+            // swal({
+            //     title: `Player creado exitosamente!`,
+            //     icon: "success",
+            //     button: 'Continuar'
+            // }).then(function (isConfirm) {
+            //     if (isConfirm) {
+            //         navigate('/')
+            //     }
+            // })
+            
+            
+        }
     }
 
     useEffect(() => {
         errorhandler()
-        console.log(player);
+        console.log(player)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [player])
 
@@ -55,42 +105,36 @@ const CreatePlayer = () => {
             <div className="cmp-create-player">
                 <div className="cmp-create-player-avatar">
                     <div className="cmp-create-player-avatar-background">
-                        {player.avatar === 0 ?
+                        {player.avatar === '' ?
                          <img src={defaultAvatar} alt="default_avatar_img" /> 
                         :
-                        <div className="avatar-movement">
+                        <div className="avatar-movement" id='avatar-movement'>
                             <Avatar pic={player.avatar} displayName={''} />
                         </div>
                     }
-                    <span>{player.name}</span>
+                    <span>{player.nickname}</span>
                     </div>
-                    <span id="cmp-create-player-span">SELECCIONÁ TU AVATAR</span>
                 </div>
 
                 <div className="cmp-create-player-form">
 
                     <div className="create-player-form-field">
-                        <label className="cmp-create-player-label" htmlFor="name">Nombre Player</label>
-                        <input type="text" placeholder="Ingresa el nombre de tu player" onChange={handlePlayer} name="name" id="name" maxLength={16} />
+                        <label className="cmp-create-player-label" htmlFor="name">Nickname</label>
+                        <input type="text" placeholder="Ingresa el nickname de tu player" onChange={handlePlayer} name="nickname" id="name" maxLength={16} />
                     </div>
 
                     <div className="create-player-form-field">
-                        <label className="cmp-create-player-label" htmlFor="status">Seleccioná tu avatar</label>
-                        <select onChange={handlePlayer} name="avatar" id="status" defaultValue='Select'>
-                            <option disabled value='Select' ></option>
-                            <option value={1}>Avatar 1</option>
-                            <option value={2}>Avatar 2</option>
-                            <option value={3}>Avatar 3</option>
-                        </select>
+                        <label className="cmp-create-player-label" htmlFor="status">Selecciona tu avatar</label>                      
+                        <Gallery imagesList={avatarList} avatarSelected={(e) => handlePlayer(e)} />
                     </div>
                    
-                    <button className="create-player-submit" onClick={createPlayer}>CREAR PLAYER</button>
+                    <button className="create-player-submit" disabled={error ? 'disabled' : ''} onClick={createPlayer}>CREAR PLAYER</button>
 
                     {error && <p style={{ color: "white", fontWeight: "bold", marginTop: "10px" }}>Todos los campos deben ser llenados</p>}
                 </div>
             </div>
             <div id='create-player-text'>
-                <h4>Una vez que crees tu Player, no podras crear otro, con lo cual piensa mucho tu nombre y avatar!</h4>
+                <h4>Una vez que crees tu Player no podras crear otro, con lo cual piensa mucho tu nombre y avatar!</h4>
             </div>
         </div>
     )
--- src/redux/constans.js
+++ src/redux/constans.js
@@ -1,6 +1,12 @@
-export const GET_TEN_PLAYERS = 'GET_TEN_PLAYERS'
+// REDUCER CONSTS
+export const GET_TOPTEN_PLAYERS = 'GET_TOPTEN_PLAYERS'
 export const GET_TEST = 'GET_TEST'
 export const ADD_TEST = 'ADD_TEST'
+export const POST_PLAYER = 'POST_PLAYER'
+export const GET_PLAYER_BY_ID = 'GET_PLAYER_BY_ID'
+export const GET_PAGINATION = 'GET_PAGINATION'
+
+
+export const API_URL = 'http://awsback-env.eba-4zfabdzp.us-west-2.elasticbeanstalk.com'
 // export const API_URL = 'http://localhost:8080'
-export const API_URL = 'https://develop.delosandes.xyz'
 export const API_PORT = '3030'

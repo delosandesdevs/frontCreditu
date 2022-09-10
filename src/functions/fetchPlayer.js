825ffebf20451017c4cd8fa7e7902a22a975fac9
Rodrigo <rodrigoperez0109@gmail.com>
2022-09-10 15:41:37 -0300

    warnings al crear con nickname repetido

--- src/functions/fetchPlayer.js
+++ src/functions/fetchPlayer.js
@@ -1,4 +1,8 @@
-export const fetchPlayer = (player) => {
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/src/sweetalert2.scss';
+
+export const fetchPlayer = (player,setCreated) => {
+    console.log('llama')
     fetch(`${process.env.REACT_APP_API_URL}/players`, {
         method: 'POST',
         body: JSON.stringify(player),
@@ -9,12 +13,24 @@ export const fetchPlayer = (player) => {
         .then(data => data.json())
         .then(res => {
             if (res === 'El nickname ya existe' || res === 'El usuario ya tiene un player') {
-                return false
+                Swal.fire({
+                    title: `'El nickname ya existe'`,
+                    icon: 'warning',
+                    confirmButtonText: 'Continuar'
+                })
+                return
             }
-            return true
+            Swal.fire({
+                title: `¡Has creado tu Player con éxito!`,
+                icon: 'success',
+                confirmButtonText: 'Continuar'
+            }).then((result) => {
+                if(result.isConfirmed) setCreated(true)
+                return
+            })
         })
         .catch(err => {
             console.log(err)
-            return false
+            return 
         })
 }

2e6b08a2166d2f9a4670f87479c06973c1a911b1
Rodrigo <rodrigoperez0109@gmail.com>
2022-09-14 19:48:58 -0300

    merge

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'https://delosandes.xyz'
   },
   chromeWebSecurity: false
 });
